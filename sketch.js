15a2dc53828d03c0b1c17f3125fd4ea2ec84b72a
Komal Dhamu <71710063+komalthecoder@users.noreply.github.com>
2021-03-25 14:30:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,71 @@
-var hr = hour();
-var min = minute();
-var sec = second();
+var hr, min, sec;
 var secAngle, hrAngle, minAngle;
 
 function setup() {
-  createCanvas(800,400);
 
+  createCanvas(400,450);
   angleMode(DEGREES);
-
-  secAngle = map(sec,0,60,0,360);
-  hrAngle = map(hr%12,0,60,0,360);
-  minAngle = map(min,0,60,0,360);
 
 }
 
 function draw() {
   background(0,0,0);  
 
-  angleMode(DEGREES);
+  translate(200,200);
+  rotate(-90);
+
+  //Calculating time using a predefined function from p5.js
+  hr = hour();
+  min = minute();
+  sec = second();
+
+  //To create rotation
+  secAngle = map(sec, 0, 60, 0, 360);
+  minAngle = map(min, 0, 60, 0, 360);
+  hrAngle = map(hr % 12, 0, 12, 0, 360);
+
+  //DRAWING THE HANDS OF THE CLOCK
+
+  //creating seconds hand
+  push();
+  rotate(secAngle);
+  stroke(0,0,255);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+
+  //creating minutes hand
+  push();
+  rotate(minAngle);
+  stroke(0,255,0);
+  strokeWeight(7);
+  line(0,0,75,0);
+  pop();
+
+  //creating hours hand
+  push();
+  rotate(hrAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,50,0);
+  pop();
+
+  stroke(255,0,255);
+  point(0,0);
+  strokeWeight(7);
+  noFill();
+
+  //arc for seconds hand
+  stroke("blue");
+  arc(0,0,300,300,0,secAngle);
+
+  //arc for minutes hand
+  stroke(0,255,0);
+  arc(0,0,280,280,0,minAngle);
+
+  //arc for hours hand
+  stroke("red");
+  arc(0,0,260,260,0,hrAngle);
+
   drawSprites();
 }
